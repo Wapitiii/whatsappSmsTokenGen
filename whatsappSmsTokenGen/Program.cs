de768e1e578cdf835cb58d26354243919fcaa79e
Wapitiii <39195287+Wapitiii@users.noreply.github.com>
2024-05-15 17:12:41 +0200

    Projektdateien hinzufügen.

---
+++ whatsappSmsTokenGen/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace whatsappSmsTokenGen
+{
+    internal class Program
+    {
+        private static readonly string _KEY = "eQV5aq/Cg63Gsq1sshN9T3gh+UUp0wIw0xgHYT1bnCjEqOJQKCRrWxdAe2yvsDeCJL+Y4G3PRD2HUF7oUgiGo8vGlNJOaux26k+A2F3hj8A=";
+        private static readonly string _SIGNATURE = "MIIDMjCCAvCgAwIBAgIETCU2pDALBgcqhkjOOAQDBQAwfDELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExFDASBgNVBAcTC1NhbnRhIENsYXJhMRYwFAYDVQQKEw1XaGF0c0FwcCBJbmMuMRQwEgYDVQQLEwtFbmdpbmVlcmluZzEUMBIGA1UEAxMLQnJpYW4gQWN0b24wHhcNMTAwNjI1MjMwNzE2WhcNNDQwMjE1MjMwNzE2WjB8MQswCQYDVQQGEwJVUzETMBEGA1UECBMKQ2FsaWZvcm5pYTEUMBIGA1UEBxMLU2FudGEgQ2xhcmExFjAUBgNVBAoTDVdoYXRzQXBwIEluYy4xFDASBgNVBAsTC0VuZ2luZWVyaW5nMRQwEgYDVQQDEwtCcmlhbiBBY3RvbjCCAbgwggEsBgcqhkjOOAQBMIIBHwKBgQD9f1OBHXUSKVLfSpwu7OTn9hG3UjzvRADDHj+AtlEmaUVdQCJR+1k9jVj6v8X1ujD2y5tVbNeBO4AdNG/yZmC3a5lQpaSfn+gEexAiwk+7qdf+t8Yb+DtX58aophUPBPuD9tPFHsMCNVQTWhaRMvZ1864rYdcq7/IiAxmd0UgBxwIVAJdgUI8VIwvMspK5gqLrhAvwWBz1AoGBAPfhoIXWmz3ey7yrXDa4V7l5lK+7+jrqgvlXTAs9B4JnUVlXjrrUWU/mcQcQgYC0SRZxI+hMKBYTt88JMozIpuE8FnqLVHyNKOCjrh4rs6Z1kW6jfwv6ITVi8ftiegEkO8yk8b6oUZCJqIPf4VrlnwaSi2ZegHtVJWQBTDv+z0kqA4GFAAKBgQDRGYtLgWh7zyRtQainJfCpiaUbzjJuhMgo4fVWZIvXHaSHBU1t5w//S0lDK2hiqkj8KpMWGywVov9eZxZy37V26dEqr/c2m5qZ0E+ynSu7sqUD7kGx/zeIcGT0H+KAVgkGNQCo5Uc0koLRWYHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXdKtOrNTQcc0e+t";
+        private static readonly string _MD5_CLASSES = "Lo0JIjOO7WlWxyy9A0AsvQ==";
+        private static readonly string _VERSION = "2.24.6.78";
+        static void Main(string[] args)
+        {
+
+            // Create an instance of TokenGenerator
+            TokenGenerator tokenGenerator = new TokenGenerator(_KEY, _SIGNATURE, _MD5_CLASSES);
+
+            // Example phone number
+            Console.WriteLine("Please enter your phone number (example: 491234567890):");
+            string phoneNumber = Console.ReadLine();
+
+            // Call the GetToken method to generate the token
+            string token = tokenGenerator.GetToken(phoneNumber);
+
+            // Print the generated token
+            Console.WriteLine("Generated Token: " + token);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ whatsappSmsTokenGen/TokenGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace whatsappSmsTokenGen
+{
+    public class TokenGenerator
+    {
+        private byte[] _KEY;
+        private byte[] _SIGNATURE;
+        private byte[] _MD5_CLASSES;
+
+        public TokenGenerator(string key, string signature, string md5Classes)
+        {
+            _KEY = Convert.FromBase64String(key);
+            _SIGNATURE = Convert.FromBase64String(signature);
+            _MD5_CLASSES = Convert.FromBase64String(md5Classes);
+        }
+
+        public string GetToken(string phoneNumber)
+        {
+            /*
+            byte[] keyDecoded = Convert.FromBase64String(_KEY);
+            byte[] sigDecoded = Convert.FromBase64String(_SIGNATURE);
+            byte[] clsDecoded = Convert.FromBase64String(_MD5_CLASSES);
+            byte[] data = Combine(sigDecoded, clsDecoded, Encoding.UTF8.GetBytes(phoneNumber));
+            */
+
+            byte[] data = _SIGNATURE.Concat(_MD5_CLASSES).Concat(Encoding.UTF8.GetBytes(phoneNumber)).ToArray();
+
+            byte[] opad = new byte[64];
+            byte[] ipad = new byte[64];
+            for (int i = 0; i < 64; i++)
+            {
+                opad[i] = (byte)(0x5C ^ _KEY[i]);
+                ipad[i] = (byte)(0x36 ^ _KEY[i]);
+            }
+
+            using (SHA1 sha1 = new SHA1Managed())
+            {
+                byte[] subHash = sha1.ComputeHash(ipad.Concat(data).ToArray());
+                byte[] hash = sha1.ComputeHash(opad.Concat(subHash).ToArray());
+                string result = Convert.ToBase64String(hash);
+                return result;
+            }
+        }
+    }
+}
